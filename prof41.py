67b4c38b051a586b88584c055dd35e3c7cd549ea
lefort999 <jlefort251@gmail.com>
2025-11-11 15:16:42 +0100

    Fix indentation for app.run in prof41.py

--- prof41.py
+++ prof41.py
@@ -124,5 +124,5 @@ def recherche():
                            caracteristique=CARACTERISTIQUE.keys())
 
 if __name__ == "__main__":
-   port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
